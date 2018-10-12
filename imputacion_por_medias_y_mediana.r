dbda9fda61808acb9f1f2e4b3bde62f8451f78c7
Talín Edmundo <tlnedworld@gmail.com>
2018-10-11 21:19:07 -0500

    Update imputacion_por_medias_y_mediana.r

--- imputacion_por_medias_y_mediana.r
+++ imputacion_por_medias_y_mediana.r
@@ -5,13 +5,8 @@ library(dplyr)
 CalibrationMissingData <- read_excel("C:/Users/junio/Desktop/Unidad 2/CalibrationMissingData.xlsx", 
                                      col_types = c("text", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", 
                                                    "numeric", "numeric"))
-"
-#Cuantos datos nos faltan?
-colSums(is.na(CalibrationMissingData)) #Cuantos en cada variable?
-sum(is.na(CalibrationMissingData))
-"
 
-#Estos tendrán los valores ya inputados
+#Estos tendrÃ¡n los valores ya inputados
 vectorImputados <- CalibrationMissingData[, -1]
 vectorImputadosMedianas <- CalibrationMissingData[, -1]
 
@@ -33,31 +28,11 @@ for (i in 2:longitud) {
   vectorMedianas[i-1] <- median( CalibrationMissingData[i] %>% unlist ) #metemos la mediana
 }
 
-"
-View(varNormalizados)
-View(datosExel)
-View(datosNormalizado)
-class(datosNormalizado)
-length(varNormalizados$V1)
-numRenglones<- length(datosExel[1] %>% unlist) #<- RESOLVIENDO ESTO RESOLVI LO DE HASTA ABAJO, quitando bastantes lineas de código
-numRenglones
-"
-
 #hacemos el reemplazo
 vectorImputados 
 vectorMedias
 vectorMedianas
 
-"
-#COMO LOS PROS EN R lo harían
-vectorImputados$C_SIO2[is.na(vectorImputados$C_SIO2)]= vectorMedias[1]
-vectorImputados$C_TIO2[is.na(vectorImputados$C_TIO2)]= vectorMedias[1]
-vectorImputados$C_AL2O3[is.na(vectorImputados$C_AL2O3)]= vectorMedias[1]
-
-*** otras 20 lineas de código, suponiendo que siempre trabajaremos con un exel de 20 renglones
-"
-
-#Como el barrio pesado lo hace
 for (i in 1:otraLongitud) {
   vectorImputados[i][ is.na(vectorImputados[i]) ]= vectorMedias[i] #para el de medias
   vectorImputadosMedianas[i][ is.na(vectorImputadosMedianas[i]) ]= vectorMedianas[i]#para el de medianas
